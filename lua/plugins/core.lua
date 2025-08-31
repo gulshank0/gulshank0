22c7b9192263296b420fabaf3a29046b91697f27
gulshank0 <gulshanbahadur002@gmail.com>
2025-08-31 23:57:36 +0530

    changed whole file

---
+++ lua/plugins/core.lua
@@ -0,0 +1,57 @@
+return {
+  -- Add catppuccin plugin first
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      show_end_of_buffer = false,
+      term_colors = false,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false,
+      },
+    },
+  },
+  -- Then configure LazyVim to use it
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night", -- Use tokyonight instead
+    },
+  },
+}
